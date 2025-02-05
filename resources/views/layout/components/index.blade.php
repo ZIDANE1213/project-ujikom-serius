6f9e19b14ab847379ee5d5dec0967e983e90f363
ZIDANE1213 <zidaneattakil455@gmail.com>
2025-02-05 23:42:00 +0700

    Add files via upload

---
+++ resources/views/layout/components/index.blade.php
@@ -0,0 +1,19 @@
+@extends('layout.main')
+
+
+@section('header')
+<div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>produk</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">produk</li>
+            </ol>
+          </div>
+@endsection
+
+@section('content')
+yeah
+@endsection
